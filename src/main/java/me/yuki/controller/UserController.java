f5e079c61c67a092b2f2b82b705a0800525d4837
Ayakura Yuki <sakurajuzi@live.com>
2017-04-18 13:57:45 +0800

    login and logout requests

--- src/main/java/me/yuki/controller/UserController.java
+++ src/main/java/me/yuki/controller/UserController.java
@@ -5,9 +5,9 @@ import me.yuki.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by Ayakura Yuki on 2017/4/10.
@@ -18,20 +18,27 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping("/auth/login/{username}/{password}")
-	public String login(@PathVariable String username, @PathVariable String password, Model model) {
+	@PostMapping("/auth/login")
+	public String login(String username, String password, Model model, HttpSession session) {
 		User user = new User();
 		user.setUsername(username);
 		user.setPassword(password);
 		User confirm = userService.loginAuthorization(user);
 		if (confirm != null) {
 			model.addAttribute("user", confirm);
+			session.setAttribute("user", confirm);
 			return "success";
 		}
 		else {
 			model.addAttribute("username", username);
 			return "error";
 		}
+	}
+
+	@GetMapping("/auth/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user");
+		return "login";
 	}
 
 	@RequestMapping("/auth/loginByJson/{username}/{password}")
